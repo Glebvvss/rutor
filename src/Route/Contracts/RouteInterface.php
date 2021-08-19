d78fbe33f3340cfcc964135b2f9c47d7519bec0e
Gleb Vasylenko <hleb.v@platinumlist.pro>
2021-08-19 12:19:44 +0300

    FIX: Add toUrl method to RouteInterface declaration

--- src/Route/Contracts/RouteInterface.php
+++ src/Route/Contracts/RouteInterface.php
@@ -7,4 +7,6 @@ use Ruter\Request\RequestInterface;
 interface RouteInterface
 {
     public function match(RequestInterface $request): MatchInterface;
+
+    public function toUrl(array $params): string;
 }
